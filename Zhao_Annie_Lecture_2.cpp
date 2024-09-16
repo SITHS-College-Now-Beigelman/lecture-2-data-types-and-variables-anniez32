730d1e5d659567948b61ac589fecbf1a6e0a4bff
anniez32 <anniez32@nycstudents.net>
2024-09-16 16:32:33 -0400

    lecture 2

---
+++ Zhao_Annie_Lecture_2.cpp
@@ -0,0 +1,28 @@
+// Annie Zhao
+// Lecture 2
+// September 16, 2024
+
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+int main () {
+
+    // creating variables
+    int inte = 2;   //integer 
+    float floate = 3;
+    double doublee = 4;
+    char chare = 'A';
+    bool boole = false;
+    string stringe = "hello";
+    cout << "int: " << inte << endl;
+    cout << "float: " << floate << endl;
+    cout << "double: " << doublee << endl;
+    cout << "char: " << chare << endl;
+    cout << "bool: " << boole << endl;
+    cout << "string: " << stringe << endl;
+    
+
+    return 0;
+}
